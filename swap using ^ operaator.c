4f26574f3abe3e181af505c6e1e47e395039e9ab
sanjeev65 <35800660+sanjeev65@users.noreply.github.com>
2018-03-15 10:38:18 +0000

    Update swap using ^ operaator.c

--- swap using ^ operaator.c
+++ swap using ^ operaator.c
@@ -1,13 +1,13 @@
 #include<conio.h>
 void main()
 {
-  int a,b;
+  int x,y;
   printf("enter the values of a and b \t:");
-  scanf("%d%d",&a,&b);
-  printf(" the before swaping numbers %d\t%d\n",a,b);
- a=a^b;
- b=a^b;
- a=a^b;
+  scanf("%d%d",&x,&y);
+  printf(" the before swaping numbers %d\t%d\n",x,y);
+ x=x^y;
+ y=x^y;
+ x=x^y;
 
-  printf("the after swaping numbers are %d\t%d\n",a,b);
+  printf("the after swaping numbers are %d\t%d\n",x,y);
 }
